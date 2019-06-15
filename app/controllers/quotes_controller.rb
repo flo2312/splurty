09b68be5a413c19ce80670069e5f3e09f13046e0
Matthew Flores <flo2312@gmail.com>
2019-06-15 17:50:54 +0000

    WK2LS13COMPLETE

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,11 +1,20 @@
 class QuotesController < ApplicationController
   def index
-    @quote = Quote.order("Random()").first
+    @quote = Quote.order("RANDOM()").first
   end
 
   def new
     @quote = Quote.new
   end
+
+  def create
+    Quote.create(quote_params)
+    redirect_to rott_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
-
-
